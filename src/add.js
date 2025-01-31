e664492c1bb3297ffb2aa146e8e61b67c15fb101
Luis Oballos Mancini <luis.oballosm@gmail.com>
2025-01-31 19:14:04 +0000

    Second commit

--- src/add.js
+++ src/add.js
@@ -1,22 +1,21 @@
-const gridView = document.getElementById('gridView');
+/* const gridView = document.getElementById('gridView');
 const feedView = document.getElementById('feedView');
 const gridViewBtn = document.getElementById('gridViewBtn');
 const feedViewBtn = document.getElementById('feedViewBtn');
 const createBtn = document.getElementById('createBtn');
 
-gridView.classList.add('hide');
-feedViewBtn.classList.add('text-primary');
+gridViewBtn.classList.remove('text-primary');
 
 gridViewBtn.addEventListener('click', () => {
-    gridView.classList.remove('hide');
-    feedView.classList.add('hide');
+    gridView.classList.add('hide');
+    feedView.classList.remove('hide');
     feedViewBtn.classList.remove('text-primary');
     gridViewBtn.classList.add('text-primary');
 });
 
 feedViewBtn.addEventListener('click', () => {
-    feedView.classList.remove('hide');
-    gridView.classList.add('hide');
+    feedView.classList.add('hide');
+    gridView.classList.remove('hide');
     gridViewBtn.classList.remove('text-primary');
     feedViewBtn.classList.add('text-primary');
 });
@@ -24,4 +23,4 @@ feedViewBtn.addEventListener('click', () => {
 createBtn.addEventListener('click', function() {
     var createPostModal = new bootstrap.Modal(document.getElementById('createPostModal'));
     createPostModal.show();
-});
+}); */
